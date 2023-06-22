e91df05caabb57b5f62610f5bc2f192a1aa5932a
chitra <chmanu.chinu@gmail.com>
2023-06-22 11:39:15 +0000

    udate jenkins

--- provider.tf
+++ provider.tf
@@ -1,4 +1,14 @@
+terraform {
+  required_providers {
+     jenkins = {
+       source = "registry.terraform.io/taiidani/jenkins"
+     }
+  }
+}
+
+
 provider "jenkins" {
+
   server_url = "http://172.31.3.199:8080" 
   username   = data.aws_ssm_parameter.jenkins_user.value           
   password   = data.aws_ssm_parameter.jenkins_pass.value          
