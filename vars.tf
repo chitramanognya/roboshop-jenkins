2a37f3535420dac6ab4b7eca33bbbe625b5b02d7
chitra <chmanu.chinu@gmail.com>
2023-06-26 07:04:22 +0000

    update jenkins

--- vars.tf
+++ vars.tf
@@ -1,11 +1,19 @@
 variable "folders"{
     
- default = [ "infra" ]
+ default = [ "infra", "CI-Pipeline" ]
  }
  
  variable "jobs" {
      default = [
-      { name = "roboshop", folder = "infra", repo_url = "https://github.com/chitramanognya/roboshop-infra" }
-     ]
+      { name = "roboshop", folder = "infra", repo_url = "https://github.com/chitramanognya/roboshop-infra" },
+      { name = "frontend", folder = "CI-Pipeline", repo_url = "https://github.com/chitramanognya/roboshop-infra" },
+      { name = "cart", folder = "CI-Pipeline", repo_url = "https://github.com/chitramanognya/roboshop-infra" },
+      { name = "catalogue", folder = "CI-Pipeline", repo_url = "https://github.com/chitramanognya/roboshop-infra" }, 
+      { name = "user", folder = "CI-Pipeline", repo_url = "https://github.com/chitramanognya/roboshop-infra" }, 
+      { name = "shipping", folder = "CI-Pipeline", repo_url = "https://github.com/chitramanognya/roboshop-infra" }, 
+      { name = "payment", folder = "CI-Pipeline", repo_url = "https://github.com/chitramanognya/roboshop-infra" } 
+       
+       
+       ]
  }
  
